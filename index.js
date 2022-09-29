de95b375cbf7fca1da8992d54d1f3e5d38e1b317
KristenSantos <Kristensxntos@gmail.com>
2022-09-29 18:40:37 +0000

    finished add function

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ untilNum(5);
 function multiply (num1, num2){
    return num1 * num2}
    console.log (multiply(2,2))
+   
+   function add (num1, num2){
+           if ( num1 === num2 ){
+               return (num1 + num2)*(3)
+           } else{
+               return num1 + num2
+           }
+       
+   } 
+   console.log(add(2, 2))
+ 
